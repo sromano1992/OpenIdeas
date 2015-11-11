8d5a07b74c6555cabbfc00598095310d88a12567
s.romano1992 <s.romano1992@gmail.com>
2015-11-11 16:10:48 +0100

    test

--- idea.php
+++ idea.php
@@ -6,7 +6,7 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <!DOCTYPE HTML>
 <html>
 <head>
-<title> Storage ui kit Flat bootstrap Responsive  Website Template | Home :: w3layouts</title>
+<title>  ui kit Flat bootstrap Responsive  Website Template | Home :: w3layouts</title>
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="js/jquery-1.11.0.min.js"></script>
